b6de8652fe1b9893221ddf6980dfa2bfa8c0fddf
Timothy Thompsen <TimmyThompsen@Timothys-MacBook-Pro.local>
2017-11-04 08:22:45 -0700

    Commit friend finder app

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friends = [
+  {
+    name: 'Kyah',
+    photo: 'http://cdn.akc.org/Marketplace/Breeds/Siberian_Husky_SERP.jpg',
+    scores: [
+      '1',
+      '5',
+      '5',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Kira',
+    photo: 'https://ampuppies.com/wp-content/uploads/bb-plugin/cache/am-siberian-husky-puppies-300x300-circle.jpg',
+    scores: [
+      '2',
+      '4',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '2',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Snowball',
+    photo: 'http://ozon4life.com/data/out/68/image-160124002.jpg',
+    scores: [
+      '4',
+      '3',
+      '5',
+      '1',
+      '1',
+      '5',
+      '5',
+      '1',
+      '1',
+      '1'
+    ]
+  },
+  {
+    name: 'Rico',
+    photo: 'http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1463780674/GettyImages-498601344.jpg?itok=dADMc_vO',
+    scores: [
+      '5',
+      '1',
+      '2',
+      '1',
+      '3',
+      '4',
+      '1',
+      '3',
+      '3',
+      '4'
+    ]
+  },
+  {
+    name: 'Cloe',
+    photo: 'http://www.lyhyxx.com/data/out/68/4363948-husky-puppies-wallpapers.jpg',
+    scores: [
+      '1',
+      '1',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '1'
+    ]
+  },
+  {
+    name: 'Ikora',
+    photo: 'http://kindofpets.com/wp-content/uploads/2017/03/husky-newborn-puppies-with-blue-eyes-kmkkokpl.jpg',
+    scores: [
+      '1',
+      '1',
+      '5',
+      '1',
+      '3',
+      '2',
+      '5',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Max',
+    photo: 'https://breedingbusiness.com/wp-content/uploads/2016/02/miniature-husky-puppy-mini-husky-1200x778.png',
+    scores: [
+      '5',
+      '4',
+      '4',
+      '2',
+      '4',
+      '4',
+      '3',
+      '2',
+      '3',
+      '3'
+    ]
+  },
+  {
+    name: 'Bowser',
+    photo: 'https://i.pinimg.com/originals/e8/a2/4c/e8a24c79cc9233288f7db5150452d5b6.jpg',
+    scores: [
+      '5',
+      '2',
+      '3',
+      '1',
+      '5',
+      '1',
+      '3',
+      '1',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Bella',
+    photo: 'https://i.ytimg.com/vi/BXS94nW6L04/hqdefault.jpg',
+    scores: [
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5',
+      '5'
+    ]
+  },
+  {
+    name: 'Spot',
+    photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/austalian-shepherd-dog-breed-pictures/5-puppy.jpg',
+    scores: [
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1',
+      '1'
+    ]
+  }
+];
+
+module.exports = friends;
